609735b49cbe13fad2b9fbae80af72a44cc19b5c
Marcelo Santos <marcelosantospvp@gmail.com>
2026-04-01 23:55:32 -0300

    Criado exemplos N1_NC4

---
+++ N1_NC4/n1_nc4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int idade = 25;
+    int quantidade = 10;
+    float altura = 1.80;
+    double peso = 150.50;
+    char letra = "M";
+    char nome[20] = "Marcelo";
+
+}
